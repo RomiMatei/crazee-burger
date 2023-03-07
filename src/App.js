76d9d170b71dd068405579adc4218b0bf2e603ae
Romina Matei <romi3783@gmail.com>
2023-03-07 11:32:38 +0100

    Add Login page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Romi</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login />
     </div>
   );
 }
