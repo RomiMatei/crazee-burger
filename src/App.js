5aa8021c09f93862a91dbd81c8b3504d2d9d2e7d
Romina Matei <romi3783@gmail.com>
2023-03-07 15:17:27 +0100

    ✨ F01: LOGIN > Add title and form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import Login from './components/pages/Login';
+import Login from './components/pages/LoginPage';
 
 function App() {
+  // state
+
+  // comportenments
+
+  // affichage(render)
   return (
     <div>
       <Login />
