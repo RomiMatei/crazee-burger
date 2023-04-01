5a71c71c5523b7478d11ec63250d898775e661ce
Romina Matei <romi3783@gmail.com>
2023-04-01 18:59:27 +0200

    💄 F03: LOGIN > LoginForm > Replace hard coded values with Design System values

--- src/components/reusable-ui/TextInput.js
+++ src/components/reusable-ui/TextInput.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { theme } from '../../theme';
 
 export default function TextInput({ value, onChange, Icon, ...extraProps }) {
   return (
@@ -10,28 +11,28 @@ export default function TextInput({ value, onChange, Icon, ...extraProps }) {
 }
 
 const InputStyled = styled.div`
-  background-color: white;
-  border-radius: 5px;
+  background-color: ${theme.colors.white};
+  border-radius: ${theme.borderRadius.round};
   display: flex;
   align-items: center;
   padding: 18px 24px;
   margin: 18px 0;
 
   .icon {
-    font-size: 15px;
+    font-size: ${theme.fonts.size.P0};
     margin-right: 8px;
-    color: #93a2b1;
+    color: ${theme.colors.greySemiDark};
   }
 
   input {
     border: none;
-    font-size: 16px;
-    color: #17161a;
+    font-size: ${theme.fonts.size.P0};
+    color: ${theme.colors.dark};
     width: 100%;
-  }
 
-  &::placeholder {
-    background-color: white;
-    color: lightgrey;
+    &::placeholder {
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.greyMedium};
+    }
   }
 `;
