368a4963ed0738ed63da295adeaeb63f2f07a25d
Romina Matei <romi3783@gmail.com>
2023-04-03 13:48:12 +0200

    💄 F04: ORDER > Add primary color to background

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Navbar from './Navbar';
 import Main from './Main';
+import { theme } from '../../../theme';
 
 export default function OrderPage() {
   // state
@@ -22,7 +23,7 @@ export default function OrderPage() {
 }
 
 const OrderPageStayled = styled.div`
-  background: orange;
+  background: ${theme.colors.primary};
   height: 100vh;
   display: flex;
   justify-content: center;
