0f49ea2a8a6269d67b22c1208a8c1df9e7285e3b
Romina Matei <romi3783@gmail.com>
2023-04-04 18:07:59 +0200

    🚧 F05: ORDER > MENU > Add product info

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { fakeMenu2 } from '../../../../fakeData/fakeMenu.js';
+import { theme } from '../../../../theme/index.js';
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -10,30 +11,43 @@ export default function Menu() {
       {menu.map((produit) => {
         return (
           <div className="menu__produit" key={produit.id}>
-            {/* <img className="menu__produit__image" src={produit.imageSource} alt={produit.title} /> */}
-            <div className="menu__produit__title">{produit.title}</div>
-            {/* <div className="menu__produit__price">{produit.price} €</div> */}
+            <img className="menu__produit__image" src={produit.imageSource} alt={produit.title} />
+            <div className="menu__produit__info-text">
+              <div className="menu__produit__title">{produit.title}</div>
+              <div className="menu__produit__description">{produit.description}</div>
+            </div>
+            <div className="menu__produit__price">{produit.price} €</div>
+            <button className="menu__produit__button">Ajouter</button>
           </div>
         );
       })}
-      <div className="menu__title">Menu</div>
-      <div className="menu__content">Content</div>
     </MenuStyled>
   );
 }
 
 const MenuStyled = styled.div`
-  background: purple;
+  background: ${theme.colors.background_white};
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
   grid-column-gap: 20px;
   padding: 50px 50px 150px;
   justify-items: center;
+  box-shadow: 0 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   .menu__produit {
     background: red;
     width: 240px;
     height: 330px;
   }
+
+  .menu__produit__image {
+    width: 100px;
+    height: auto;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
 `;
