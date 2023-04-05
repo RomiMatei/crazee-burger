df8d0c9655e6c3aea3610c5c69f0bf8d1fc59c8e
Romina Matei <romi3783@gmail.com>
2023-04-05 10:20:24 +0200

    Revert "🚧 F05: ORDER > MENU > Add grid layout"
    This reverts commit b2b14fb231a8205c39879434df51c3327631fd6a.

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -18,7 +18,6 @@ const MainStyled = styled.div`
 
   display: grid;
   grid-template-columns: 1fr;
-  overflow-y: scroll;
 
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
@@ -27,4 +26,8 @@ const MainStyled = styled.div`
   /* .basket {
     background: pink;
   } */
+
+  .menu {
+    background: purple;
+  }
 `;
