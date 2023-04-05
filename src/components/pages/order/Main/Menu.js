9b787159358e264084432270ed6c682424aeef3e
Romina Matei <romi3783@gmail.com>
2023-04-05 09:37:23 +0200

    ♻️ F05: ORDER > MENU > Create <Product/>

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { fakeMenu2 } from '../../../../fakeData/fakeMenu.js';
 import { theme } from '../../../../theme/index.js';
+import Product from './Product.js';
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -9,17 +10,7 @@ export default function Menu() {
   return (
     <MenuStyled className="menu">
       {menu.map((produit) => {
-        return (
-          <div className="menu__produit" key={produit.id}>
-            <img className="menu__produit__image" src={produit.imageSource} alt={produit.title} />
-            <div className="menu__produit__info-text">
-              <div className="menu__produit__title">{produit.title}</div>
-              <div className="menu__produit__description">{produit.description}</div>
-            </div>
-            <div className="menu__produit__price">{produit.price} €</div>
-            <button className="menu__produit__button">Ajouter</button>
-          </div>
-        );
+        return <Product {...produit} />;
       })}
     </MenuStyled>
   );
@@ -34,20 +25,4 @@ const MenuStyled = styled.div`
   padding: 50px 50px 150px;
   justify-items: center;
   box-shadow: 0 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-  .menu__produit {
-    background: red;
-    width: 240px;
-    height: 330px;
-  }
-
-  .menu__produit__image {
-    width: 100px;
-    height: auto;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
 `;
