9ae5e570d1db4644a199296c99575bb67d97123b
Romina Matei <romi3783@gmail.com>
2023-04-11 17:23:33 +0200

    🍱 F03 : THEME > Update font-size and shadows

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,45 +1,46 @@
 const colors = {
-  dark: '#17161a',
-  incognito: '#333',
-  white: '#fff',
-  background_white: '#f5f5f7',
-  background_dark: '#292729',
-  primary: '#ffa01b',
-  green: 'green',
-  success: '#60bd4f',
-  red: '#e25549',
-  redSecondary: '#c4151c',
-  blue: '#51a7e1',
-  greyLight: '#e4e5e9',
-  greyMedium: '#a7a8ad',
-  greySemiDark: '#93a2b1',
-  greyDark: '#6f737e',
-  greyBlue: '#747b91',
-  loginLine: '#f56a2c'
-};
+  dark: "#17161a",
+  incognito: "#333",
+  white: "#fff",
+  background_white: "#f5f5f7",
+  background_dark: "#292729",
+  primary: "#ffa01b",
+  green: "green",
+  success: "#60bd4f",
+  red: "#e25549",
+  redSecondary: "#c4151c",
+  blue: "#51a7e1",
+  greyLight: "#e4e5e9",
+  greyMedium: "#a7a8ad",
+  greySemiDark: "#93a2b1",
+  greyDark: "#6f737e",
+  greyBlue: "#747b91",
+  loginLine: "#f56a2c",
+}
 
 const spacing = {
-  xxs: '4px',
-  xs: '8px',
-  sm: '12px',
-  md: '20px',
-  lg: '32px',
-  xl: '52px',
-  xxl: '84px'
-};
+  xxs: "4px",
+  xs: "8px",
+  sm: "12px",
+  md: "20px",
+  lg: "32px",
+  xl: "52px",
+  xxl: "84px",
+}
 
 const fonts = {
   size: {
-    XXXS: '8px',
-    XXS: '10px',
-    XS: '12px',
-    P0: '15px',
-    P1: '18px',
-    P2: '20px',
-    P3: '24px',
-    P4: '36px',
-    P5: '48px',
-    P6: '60px'
+    XXXS: "8px",
+    XXS: "10px",
+    XS: "12px",
+    SM: "15px",
+    P0: "16px",
+    P1: "18px",
+    P2: "20px",
+    P3: "24px",
+    P4: "36px",
+    P5: "48px",
+    P6: "60px",
   },
   weights: {
     light: 300,
@@ -47,22 +48,22 @@ const fonts = {
     medium: 500,
     semiBold: 600,
     bold: 700,
-    heavy: 800
-  }
-};
-
-const gridUnit = 8;
+    heavy: 800,
+  },
+}
+const gridUnit = 8
 const borderRadius = {
   subtle: 1,
-  round: '5px',
-  extraRound: '15px',
-  circle: '50%'
-};
+  round: "5px",
+  extraRound: "15px",
+  circle: "50%",
+}
+
 const shadows = {
-  strong: '0 8px 20px 8px rgba(0, 0, 0, 0.2) inset',
-  medium: '0 -2px 8px -2px rgb(0 0 0 / 20%)',
-  soft: '0 -2px 8px -2px rgb(0 0 0 / 10%)'
-};
+  subtle: "0px -6px 8px -2px rgba(0, 0, 0, 0.1)",
+  medium: "-8px 8px 20px 0px rgb(0 0 0 / 20%)",
+  strong: "0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset",
+}
 
 export const theme = {
   colors,
@@ -70,5 +71,5 @@ export const theme = {
   gridUnit,
   borderRadius,
   shadows,
-  spacing
-};
+  spacing,
+}
