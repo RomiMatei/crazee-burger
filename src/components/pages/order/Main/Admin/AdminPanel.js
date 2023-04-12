73ed225f34d7e22406af2d86742d9643a3e10b37
Romina Matei <romi3783@gmail.com>
2023-04-12 10:45:50 +0200

    💄 F07 : ADMIN > Add AdminTabs and AdminPanel

---
+++ src/components/pages/order/Main/Admin/AdminPanel.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { theme } from '../../../../../theme';
+
+export default function AdminPanel() {
+  return <AdminPanelStyled>AdminPanel</AdminPanelStyled>;
+}
+
+const AdminPanelStyled = styled.div`
+  border: 1px solid green;
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+  height: 250px;
+`;
