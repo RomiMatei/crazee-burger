91def8f5df1564e439818e0d2e9537e76ba97677
Romina Matei <romi3783@gmail.com>
2023-04-12 12:25:03 +0200

    💄 F07 : ADMIN > Tab > Add click and active style

--- src/components/pages/order/Main/Admin/AdminTabs.js
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
 import Tab from '../../../../reusable-ui/Tab';
 import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import { theme } from '../../../../../theme';
 
-export default function AdminTabs() {
+export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
+  const handleClick = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <AdminTabsStyled>
-      <Tab Icon={<FiChevronDown />} />
+      <Tab
+        Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
+        onClick={handleClick}
+        className={isCollapsed ? 'is-active' : ''}
+      />
     </AdminTabsStyled>
   );
 }
 
-const AdminTabsStyled = styled.div``;
+const AdminTabsStyled = styled.div`
+  display: flex;
+  padding: 0 20px;
+
+  .is-active {
+    background: ${theme.colors.background_dark};
+    border-color: ${theme.colors.background_dark};
+    color: ${theme.colors.white};
+  }
+`;
